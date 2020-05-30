49b373dc983003e60c63f477ab0bc0bff31f2eb6
Brahian Dev <bsanchezla@uninpahu.edu.co>
2020-05-30 11:10:25 -0500

    Embed powerbi

--- view/user/powerbi.php
+++ view/user/powerbi.php
@@ -3,7 +3,7 @@
   <div class="container-fluid">
       <!-- Content Row -->
       <div class="row">
-        <iframe width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=8c021bb3-afad-4d63-b171-6dd5f16b20d8&autoAuth=true&ctid=e512294f-d02c-4380-927d-e8427efc06df&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXNvdXRoLWNlbnRyYWwtdXMtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D" frameborder="0" allowFullScreen="true"></iframe>
+      <iframe width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=b885f92c-1067-46b2-baf9-dc346ec19e62&autoAuth=true&ctid=e512294f-d02c-4380-927d-e8427efc06df&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXNvdXRoLWNlbnRyYWwtdXMtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D" frameborder="0" allowFullScreen="true"></iframe>
       </div>
     <!-- /.container-fluid -->
     </div>
